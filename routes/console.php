41428e204b8cd723e9681ac9f1c8588606991283
Julien Bourdeau <julien@sigerr.org>
2017-05-18 11:44:48 +0200

    :scroll: Create `geosearch` command

--- routes/console.php
+++ routes/console.php
@@ -14,3 +14,16 @@
 Artisan::command('search {query}', function ($query) {
     dd(\App\Airport::search($query)->get());
 })->describe('Simple search for an Airport');
+
+Artisan::command('geosearch {lat} {lng} {radius}', function ($lat, $lng, $radius) {
+    dd(\App\Airport::search('', function ($algolia, $query, $options) use ($lat, $lng, $radius) {
+        $location =  [
+            'aroundLatLng' => $lat.','.$lng,
+            'aroundRadius' => $radius * 1000,
+        ];
+
+        $options = array_merge($options, $location);
+
+        return $algolia->search($query, $options);
+    })->get());
+})->describe('Simple search for an Airport');
