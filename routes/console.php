5a73d920d415adaaeaf99e33bc6b913e7f0315e0
Julien Bourdeau <julien@sigerr.org>
2017-05-18 13:16:39 +0000

    Add search inside a polygon example (`searchuk` command)

--- routes/console.php
+++ routes/console.php
@@ -31,3 +31,23 @@ Artisan::command('geosearch {lat} {lng} {radius}', function ($lat, $lng, $radius
 Artisan::command('geosearch2 {query} {lat} {lng} {radius}', function ($query, $lat, $lng, $radius) {
     dd(\App\Airport::searchAround($query, $lat, $lng, $radius)->get());
 })->describe('Simple search for an Airport, but easier');
+
+Artisan::command('searchuk {query}', function ($query) {
+    dd(\App\Airport::search($query, function ($algolia, $query, $options) {
+        $polygonAroundTheUK = [
+            52.9332312, -1.9937525, // Point 1 lat, lng
+            52.1312677, -2.0434894, // Point 2 lat, lng
+            52.7029492,-1.2530685, // Point 3 lat, lng
+            51.5262792, 0.1192389, // Point 4 lat, lng
+            48.8364598, 1.7006836, // Point 5 lat, lng
+        ];
+
+        $location = [
+            'insidePolygon' => [$polygonAroundTheUK]
+        ];
+
+        $options = array_merge($options, $location);
+
+        return $algolia->search($query, $options);
+    })->get());
+})->describe('Search for an airport in the UK');
