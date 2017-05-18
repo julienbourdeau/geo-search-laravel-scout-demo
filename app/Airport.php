9846b5834aed1aec699683704a15124da8f89883
Julien Bourdeau <julien@sigerr.org>
2017-05-18 09:41:50 +0000

    Make Airport searchable and format data

--- app/Airport.php
+++ app/Airport.php
@@ -3,7 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Airport extends Model
 {
+    use Searchable;
+
+    public function toSearchableArray()
+    {
+        $record = $this->toArray();
+
+        $record['_geoloc'] = [
+            'lat' => $record['latitude'],
+            'lng' => $record['longitude'],
+        ];
+
+        unset($record['created_at'], $record['updated_at']); // Pro tips
+        unset($record['latitude'], $record['longitude']);
+
+        return $record;
+    }
 }
